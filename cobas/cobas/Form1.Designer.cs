ab0363e24a79d08d733e62f405b4477f1c815b57
miguelgunawan <128301456+miguelgunawan@users.noreply.github.com>
2023-05-15 11:20:08 +0700

    Add files via upload

---
+++ cobas/cobas/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace cobas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.textBox_TeamNumber = new System.Windows.Forms.TextBox();
+            this.textBox_Position = new System.Windows.Forms.TextBox();
+            this.textBox_Height = new System.Windows.Forms.TextBox();
+            this.textBox_Weight = new System.Windows.Forms.TextBox();
+            this.comboBox_Nationality = new System.Windows.Forms.ComboBox();
+            this.comboBox_TeamName = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button_AddPlayer = new System.Windows.Forms.Button();
+            this.comboBox_TeamManager = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button_UpdateManager = new System.Windows.Forms.Button();
+            this.comboBox_TeamDelete = new System.Windows.Forms.ComboBox();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.button_Delete = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox_PlayerID = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Team Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nationality";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Position";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Height";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Weight";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Birthdate";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 248);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Team Name";
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Location = new System.Drawing.Point(129, 40);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(113, 20);
+            this.textBox_Name.TabIndex = 8;
+            // 
+            // textBox_TeamNumber
+            // 
+            this.textBox_TeamNumber.Location = new System.Drawing.Point(129, 70);
+            this.textBox_TeamNumber.Name = "textBox_TeamNumber";
+            this.textBox_TeamNumber.Size = new System.Drawing.Size(113, 20);
+            this.textBox_TeamNumber.TabIndex = 9;
+            // 
+            // textBox_Position
+            // 
+            this.textBox_Position.Location = new System.Drawing.Point(129, 130);
+            this.textBox_Position.Name = "textBox_Position";
+            this.textBox_Position.Size = new System.Drawing.Size(113, 20);
+            this.textBox_Position.TabIndex = 10;
+            // 
+            // textBox_Height
+            // 
+            this.textBox_Height.Location = new System.Drawing.Point(129, 161);
+            this.textBox_Height.Name = "textBox_Height";
+            this.textBox_Height.Size = new System.Drawing.Size(113, 20);
+            this.textBox_Height.TabIndex = 11;
+            // 
+            // textBox_Weight
+            // 
+            this.textBox_Weight.Location = new System.Drawing.Point(129, 189);
+            this.textBox_Weight.Name = "textBox_Weight";
+            this.textBox_Weight.Size = new System.Drawing.Size(113, 20);
+            this.textBox_Weight.TabIndex = 12;
+            // 
+            // comboBox_Nationality
+            // 
+            this.comboBox_Nationality.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_Nationality.FormattingEnabled = true;
+            this.comboBox_Nationality.Location = new System.Drawing.Point(129, 100);
+            this.comboBox_Nationality.Name = "comboBox_Nationality";
+            this.comboBox_Nationality.Size = new System.Drawing.Size(113, 21);
+            this.comboBox_Nationality.TabIndex = 15;
+            // 
+            // comboBox_TeamName
+            // 
+            this.comboBox_TeamName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_TeamName.FormattingEnabled = true;
+            this.comboBox_TeamName.Location = new System.Drawing.Point(129, 248);
+            this.comboBox_TeamName.Name = "comboBox_TeamName";
+            this.comboBox_TeamName.Size = new System.Drawing.Size(113, 21);
+            this.comboBox_TeamName.TabIndex = 16;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(130, 218);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(113, 20);
+            this.dateTimePicker1.TabIndex = 17;
+            // 
+            // button_AddPlayer
+            // 
+            this.button_AddPlayer.Location = new System.Drawing.Point(92, 321);
+            this.button_AddPlayer.Name = "button_AddPlayer";
+            this.button_AddPlayer.Size = new System.Drawing.Size(75, 23);
+            this.button_AddPlayer.TabIndex = 18;
+            this.button_AddPlayer.Text = "Add Player";
+            this.button_AddPlayer.UseVisualStyleBackColor = true;
+            this.button_AddPlayer.Click += new System.EventHandler(this.button_AddPlayer_Click);
+            // 
+            // comboBox_TeamManager
+            // 
+            this.comboBox_TeamManager.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_TeamManager.FormattingEnabled = true;
+            this.comboBox_TeamManager.Location = new System.Drawing.Point(261, 39);
+            this.comboBox_TeamManager.Name = "comboBox_TeamManager";
+            this.comboBox_TeamManager.Size = new System.Drawing.Size(188, 21);
+            this.comboBox_TeamManager.TabIndex = 19;
+            this.comboBox_TeamManager.SelectedIndexChanged += new System.EventHandler(this.comboBox_TeamManager_SelectedIndexChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(25, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Player";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(302, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(49, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Manager";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(261, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(188, 73);
+            this.dataGridView1.TabIndex = 22;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(261, 149);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(188, 237);
+            this.dataGridView2.TabIndex = 23;
+            // 
+            // button_UpdateManager
+            // 
+            this.button_UpdateManager.Location = new System.Drawing.Point(323, 392);
+            this.button_UpdateManager.Name = "button_UpdateManager";
+            this.button_UpdateManager.Size = new System.Drawing.Size(75, 23);
+            this.button_UpdateManager.TabIndex = 24;
+            this.button_UpdateManager.Text = "Update";
+            this.button_UpdateManager.UseVisualStyleBackColor = true;
+            // 
+            // comboBox_TeamDelete
+            // 
+            this.comboBox_TeamDelete.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_TeamDelete.FormattingEnabled = true;
+            this.comboBox_TeamDelete.Location = new System.Drawing.Point(455, 39);
+            this.comboBox_TeamDelete.Name = "comboBox_TeamDelete";
+            this.comboBox_TeamDelete.Size = new System.Drawing.Size(394, 21);
+            this.comboBox_TeamDelete.TabIndex = 25;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(455, 70);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(394, 316);
+            this.dataGridView3.TabIndex = 26;
+            // 
+            // button_Delete
+            // 
+            this.button_Delete.Location = new System.Drawing.Point(561, 392);
+            this.button_Delete.Name = "button_Delete";
+            this.button_Delete.Size = new System.Drawing.Size(75, 23);
+            this.button_Delete.TabIndex = 27;
+            this.button_Delete.Text = "Delete";
+            this.button_Delete.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(25, 281);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(50, 13);
+            this.label11.TabIndex = 28;
+            this.label11.Text = "Player ID";
+            // 
+            // textBox_PlayerID
+            // 
+            this.textBox_PlayerID.Location = new System.Drawing.Point(129, 281);
+            this.textBox_PlayerID.Name = "textBox_PlayerID";
+            this.textBox_PlayerID.Size = new System.Drawing.Size(113, 20);
+            this.textBox_PlayerID.TabIndex = 29;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 442);
+            this.Controls.Add(this.textBox_PlayerID);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button_Delete);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.comboBox_TeamDelete);
+            this.Controls.Add(this.button_UpdateManager);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBox_TeamManager);
+            this.Controls.Add(this.button_AddPlayer);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox_TeamName);
+            this.Controls.Add(this.comboBox_Nationality);
+            this.Controls.Add(this.textBox_Weight);
+            this.Controls.Add(this.textBox_Height);
+            this.Controls.Add(this.textBox_Position);
+            this.Controls.Add(this.textBox_TeamNumber);
+            this.Controls.Add(this.textBox_Name);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.TextBox textBox_TeamNumber;
+        private System.Windows.Forms.TextBox textBox_Position;
+        private System.Windows.Forms.TextBox textBox_Height;
+        private System.Windows.Forms.TextBox textBox_Weight;
+        private System.Windows.Forms.ComboBox comboBox_Nationality;
+        private System.Windows.Forms.ComboBox comboBox_TeamName;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button_AddPlayer;
+        private System.Windows.Forms.ComboBox comboBox_TeamManager;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button_UpdateManager;
+        private System.Windows.Forms.ComboBox comboBox_TeamDelete;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button_Delete;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox_PlayerID;
+    }
+}
+
---
+++ cobas/cobas/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace cobas
+{
+
+    public partial class Form1 : Form
+    {
+        MySqlDataAdapter mySqlDataAdapter;
+        MySqlConnection mySqlConnection;
+        MySqlDataReader mySqlDataReader;
+        MySqlCommand mySqlCommand;
+
+        string connection = "server=localhost;uid=root;pwd=;database=premier_league";
+        string query = "";
+        DataTable dtPlayer = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+            query = "";
+            mySqlConnection = new MySqlConnection(connection);
+            mySqlCommand = new MySqlCommand(query,mySqlConnection);
+        }
+
+        private void forDGV()
+        {
+            query = "select * from player";
+            mySqlConnection = new MySqlConnection(connection);
+            mySqlCommand = new MySqlCommand(query, mySqlConnection);
+            mySqlDataAdapter = new MySqlDataAdapter(mySqlCommand);
+            mySqlDataAdapter.Fill(dtPlayer);
+            dataGridView3.DataSource = dtPlayer;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                mySqlConnection.Open();
+                MySqlCommand sc = new MySqlCommand("SELECT nationality_id, nation FROM nationality n;", mySqlConnection);
+                mySqlDataReader=sc.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Columns.Add("nation", typeof(string));
+                dt.Load(mySqlDataReader);
+                comboBox_Nationality.DisplayMember = "nation";
+                comboBox_Nationality.ValueMember = "nationality_id";
+
+                comboBox_Nationality.DataSource = dt;
+                mySqlConnection.Close();
+
+                mySqlConnection.Open();
+                MySqlCommand sc1 = new MySqlCommand("SELECT team_id, team_name FROM team t;", mySqlConnection);
+                mySqlDataReader = sc1.ExecuteReader();
+                DataTable dt1 = new DataTable();
+                dt1.Columns.Add("team_name", typeof(string));
+                dt1.Load(mySqlDataReader);
+                comboBox_TeamName.DisplayMember = "team_name";
+                comboBox_TeamName.ValueMember = "team_id";
+                comboBox_TeamName.DataSource = dt1;
+                mySqlConnection.Close();
+
+                mySqlConnection.Open();
+                MySqlCommand sc2 = new MySqlCommand("SELECT team_name FROM team t;", mySqlConnection);
+                mySqlDataReader = sc2.ExecuteReader();
+                DataTable dt2 = new DataTable();
+                dt2.Columns.Add("team_name", typeof(string));
+                dt2.Load(mySqlDataReader);
+                comboBox_TeamManager.DisplayMember = "team_name";
+                comboBox_TeamManager.DataSource = dt2;
+                mySqlConnection.Close();
+            }
+            catch (Exception)
+            {
+
+                
+            }
+            forDGV();
+        }
+
+        private void button_AddPlayer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mySqlConnection.Open();
+                MySqlCommand sc = new MySqlCommand("insert into PLAYER values('" + textBox_PlayerID.Text + "','" + textBox_TeamNumber.Text + "','" + textBox_Name.Text + "','" + comboBox_Nationality.SelectedValue.ToString() + "','" + textBox_Position.Text + "','" + textBox_Height.Text + "','" + textBox_Weight.Text + "','" + dateTimePicker1.Text + "','" + comboBox_TeamName.SelectedValue.ToString() +"','"+ '1' + "','" + '0' + "')", mySqlConnection);
+                sc.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                forDGV();
+                mySqlConnection.Close();
+            }
+
+
+
+        }
+
+        private void comboBox_TeamManager_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dtManager = new DataTable();
+            mySqlConnection = new MySqlConnection(connection);
+            query = $"SELECT m.manager_name, t.team_name,birthdate,nation\r\nFrom team t\r\nJoin manager m\r\non t.manager_id=m.manager_id\r\njoin nationality n\r\non m.nationality_id=n.nationality_id\r\nwhere t.team_name = '{comboBox_TeamManager.Text}';";
+            mySqlCommand = new MySqlCommand(query, mySqlConnection);
+            mySqlDataAdapter = new MySqlDataAdapter(mySqlCommand);
+            mySqlDataAdapter.Fill(dtManager);
+            dataGridView1.DataSource = dtManager;
+
+            DataTable dtManagerNotWorking = new DataTable();
+            mySqlConnection = new MySqlConnection(connection);
+            query = $"SELECT manager_name,nation,birthdate\r\nFROM manager m\r\njoin nationality n\r\non m.nationality_id=n.nationality_id\r\nwhere working = 0;";
+            mySqlCommand = new MySqlCommand(query, mySqlConnection);
+            mySqlDataAdapter = new MySqlDataAdapter(mySqlCommand);
+            mySqlDataAdapter.Fill(dtManagerNotWorking);
+            dataGridView2.DataSource = dtManagerNotWorking;
+        }
+    }
+}
